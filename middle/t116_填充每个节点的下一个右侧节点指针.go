771554c7242c198c26cef0156a4773a7b64d57bb
Firewine <xyjworkgame@gmail.com>
2022-02-16 15:08:18 +0800

    修改错误

--- middle/t116_填充每个节点的下一个右侧节点指针.go
+++ middle/t116_填充每个节点的下一个右侧节点指针.go
@@ -39,13 +39,21 @@ func connect2(root *Node) *Node {
 		return root
 	}
 
-	for leftmost := root; leftmost != nil; leftmost = leftmost.Left {
+	// 每次循环从该层的最左侧节点开始
+	for leftmost := root; leftmost.Left != nil; leftmost = leftmost.Left {
+		// 通过 Next 遍历这一层节点，为下一层的节点更新 Next 指针
 		for node := leftmost; node != nil; node = node.Next {
+			// 左节点指向右节点
 			node.Left.Next = node.Right
+
+			// 右节点指向下一个左节点
 			if node.Next != nil {
 				node.Right.Next = node.Next.Left
 			}
 		}
 	}
+
+	// 返回根节点
 	return root
+
 }

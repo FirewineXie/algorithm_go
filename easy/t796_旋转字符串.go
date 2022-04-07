400db476359c96363978fc72eb53d8c836dfaeb1
Firewine <xyjworkgame@gmail.com>
2022-04-07 20:41:44 +0800

    完成t796

---
+++ easy/t796_旋转字符串.go
@@ -0,0 +1,10 @@
+package easy
+
+import "strings"
+
+//
+func rotateString(s string, goal string) bool {
+
+	return len(s) == len(goal) && strings.Contains(s+s, goal)
+
+}
---
+++ easy/t796_旋转字符串_test.go
@@ -0,0 +1,11 @@
+package easy
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_rotateString(t *testing.T) {
+	fmt.Println(rotateString("abcde", "abced"))
+
+}
